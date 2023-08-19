d1ed50ac33122a1bfb043ada9e132b666eff04f1
SubhasishSarkar <sarkarsubhasish207@gmail.com>
2023-08-19 17:59:06 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
-
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugin  = {
+    "name": "MovieApp",
+    "short_name": "MovieApp",
+    "dir": "ltr",
+    "lang": "en-US",
+    "orientation": "any",
+    "start_url": "/",
+    "background_color": "#fff",
+    "theme_color": "#fff",
+    "display": "standalone",
+    "description": "A highly scalable & plug-able, Progressive Web Application foundation with the best Developer Experience.",
+    "icons": [
+        {
+            "src": "/icon512.png",
+            "sizes": "512x512",
+            "type" : "image/png"
+        },
+    ]
+}
 const dotEnvReplacement = (env) => {
     const replacement = Object.entries(env).reduce((obj, [key, val]) => {
         obj[`process.env.${key}`] = `"${val}"`
@@ -18,6 +37,6 @@ const dotEnvReplacement = (env) => {
 export default defineConfig(() => {
     const env = dotenv.config({ path: './.env' }).parsed
     return {
-        plugins: [react(), dotEnvReplacement(env)],
+        plugins: [react(), dotEnvReplacement(env), VitePWA(manifestForPlugin)],
     }
 })
